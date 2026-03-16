c9db5f713ef28a7afc17b96676a1b55bec4d1dfd
JH2Yoon <jyun0045@gmail.com>
2026-03-16 16:10:36 +0900

    init commit

---
+++ src/test/java/com/example/smartenroll/SmartEnrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartenroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEnrollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
